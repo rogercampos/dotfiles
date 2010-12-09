1ce243095e152ef0334e3fb66a3121855694e086
Roger Campos <roger@itnig.net>
2010-12-09 11:07:43 +0100

    adding useful aliases form mislav dotfiles

--- ruby/aliases.bash
+++ ruby/aliases.bash
@@ -1,1 +1,18 @@
 alias be='bundle exec'
+
+# from mislav dotfiles: https://github.com/mislav/dotfiles
+
+function cdgem {
+  cd `gem env gemdir`/gems
+  cd `ls | grep $1 | sort | tail -1`
+}
+
+function gemdoc {
+  GEMDIR=`gem env gemdir`/doc
+  gnome-open $GEMDIR/`ls $GEMDIR | grep $1 | sort | tail -1`/rdoc/index.html
+}
+
+function vimgem {
+  GEMDIR=`gem env gemdir`/gems
+  vim $GEMDIR/`ls $GEMDIR | grep $1 | sort | tail -1`
+}
