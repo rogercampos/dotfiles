36dcfe7b6f15ac11c8b80485025e594f11bb30e4
Roger Campos <roger@itnig.net>
2011-03-24 01:06:15 +0100

    git pull with rebase

--- git/aliases.bash
+++ git/aliases.bash
@@ -1,7 +1,7 @@
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias glc="git log --graph --color --date=relative --stat"
 alias gph='git push'
-alias gpl='git pull'
+alias gpl='git pull --rebase'
 alias gd='git diff'
 alias gc='git commit -v'
 alias gca='git commit -a'
